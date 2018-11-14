3c2bf9e27df3d17b578583dce4068c95fbc86ec9
nagaraju11 <nraj0429@gmail.com>
2018-11-14 13:01:06 -0800

    handy

---
+++ Desktop/handy.sql
@@ -0,0 +1,331 @@
+
+-- show running queries (9.2)
+
+SELECT pid, age(clock_timestamp(), query_start), usename, query 
+FROM pg_stat_activity 
+WHERE query != '<IDLE>' AND query NOT ILIKE '%pg_stat_activity%' 
+ORDER BY query_start desc;
+
+-- kill running query
+SELECT pg_cancel_backend(procpid);
+
+-- kill idle query
+SELECT pg_terminate_backend(procpid);
+
+-- vacuum command
+
+VACUUM (VERBOSE, ANALYZE);
+--- to check list of extennions 
+
+select * from pg_avaialable_extensions order by name;
+-- all databases and their sizes
+
+select * from pg_stat_activity where current_query not like '<%';
+
+-- all database users
+select * from pg_user;
+
+-- all tables and their size, with/without indexes
+select datname, pg_size_pretty(pg_database_size(datname))
+from pg_database
+order by pg_database_size(datname) desc;
+--- Hishest disk space used table
+SELECT 
+       relname AS "table_name", 
+       pg_size_pretty(pg_table_size(C.oid)) AS "table_size" 
+FROM 
+       pg_class C 
+LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace) 
+WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND nspname !~ '^pg_toast' AND relkind IN ('r') 
+ORDER BY pg_table_size(C.oid) 
+DESC LIMIT 1;
+
+-- cache hit rates (should not be less than 0.99)
+SELECT sum(heap_blks_read) as heap_read, sum(heap_blks_hit)  as heap_hit, (sum(heap_blks_hit) - sum(heap_blks_read)) / sum(heap_blks_hit) as ratio
+FROM pg_statio_user_tables;
+
+-- table index usage rates (should not be less than 0.99)
+SELECT relname, 100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used, n_live_tup rows_in_table
+FROM pg_stat_user_tables 
+ORDER BY n_live_tup DESC;
+
+-- how many indexes are in cache
+SELECT sum(idx_blks_read) as idx_read, sum(idx_blks_hit)  as idx_hit, (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit) as ratio
+FROM pg_statio_user_indexes;
+
+-- Dump database on remote host to file
+$ pg_dump -U username -h hostname databasename > dump.sql
+
+-- Import dump into existing database
+$ psql -d newdb -f dump.sql
+
+-- transfer tables one db to another db
+
+$pg_dump -h hostname -p port -u username -t tablename databasename | psql -h hostname -p port -u username targetdb_name
+
+explain select * from table;
+--- last vacuum time --
+select * from pg_stat_user_tables;
+VACUUM table;
+VACUUM FULL table;
+--- server vacuum --
+VACUUM FULL VERBOSE;
+
+--- list of tables that autovacuum sees as eligible for vacuuming
+
+  WITH vbt AS (SELECT setting AS autovacuum_vacuum_threshold FROM 
+pg_settings WHERE name = 'autovacuum_vacuum_threshold')
+    , vsf AS (SELECT setting AS autovacuum_vacuum_scale_factor FROM 
+pg_settings WHERE name = 'autovacuum_vacuum_scale_factor')
+    , fma AS (SELECT setting AS autovacuum_freeze_max_age FROM 
+pg_settings WHERE name = 'autovacuum_freeze_max_age')
+    , sto AS (select opt_oid, split_part(setting, '=', 1) as param, 
+split_part(setting, '=', 2) as value from (select oid opt_oid, 
+unnest(reloptions) setting from pg_class) opt)
+SELECT
+    '"'||ns.nspname||'"."'||c.relname||'"' as relation
+    , pg_size_pretty(pg_table_size(c.oid)) as table_size
+    , age(relfrozenxid) as xid_age
+    , coalesce(cfma.value::float, autovacuum_freeze_max_age::float) 
+autovacuum_freeze_max_age
+    , (coalesce(cvbt.value::float, autovacuum_vacuum_threshold::float) 
++ coalesce(cvsf.value::float,autovacuum_vacuum_scale_factor::float) * 
+c.reltuples) as autovacuum_vacuum_tuples
+    , n_dead_tup as dead_tuples
+FROM pg_class c join pg_namespace ns on ns.oid = c.relnamespace
+join pg_stat_all_tables stat on stat.relid = c.oid
+join vbt on (1=1) join vsf on (1=1) join fma on (1=1)
+left join sto cvbt on cvbt.param = 'autovacuum_vacuum_threshold' and 
+c.oid = cvbt.opt_oid
+left join sto cvsf on cvsf.param = 'autovacuum_vacuum_scale_factor' and 
+c.oid = cvsf.opt_oid
+left join sto cfma on cfma.param = 'autovacuum_freeze_max_age' and 
+c.oid = cfma.opt_oid
+WHERE c.relkind = 'r' and nspname <> 'pg_catalog'
+and (
+    age(relfrozenxid) >= coalesce(cfma.value::float, 
+autovacuum_freeze_max_age::float)
+    or
+    coalesce(cvbt.value::float, autovacuum_vacuum_threshold::float) + 
+coalesce(cvsf.value::float,autovacuum_vacuum_scale_factor::float) * 
+c.reltuples <= n_dead_tup
+   -- or 1 = 1
+)
+ORDER BY age(relfrozenxid) DESC LIMIT 50;
+
+--- Determining if Autovacuum Is Currently Running and For How Long
+
+SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query
+FROM pg_stat_activity 
+WHERE upper(query) like '%VACUUM%' 
+ORDER BY xact_start;
+-- scheam sizes 
+SELECT schema_name, pg_size_pretty(sum(table_size)::bigint) as disk_space, 
+      ((sum(table_size)::bigint) / 1024) / 1024 as size_MB 
+      FROM (SELECT pg_catalog.pg_namespace.nspname
+            as schema_name, pg_relation_size(pg_catalog.pg_class.oid) 
+            as table_size 
+      FROM pg_catalog.pg_class JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid) t
+WHERE t.schema_name not in ('information_schema','pg_catalog','pg_toast') 
+GROUP BY schema_name 
+ORDER BY schema_name;
+
+-- Buffercache 
+SELECT c.relname, count(*) AS buffers
+             FROM dbadmin.pg_buffercache b INNER JOIN pg_class c
+             ON b.relfilenode = pg_relation_filenode(c.oid) AND
+                b.reldatabase IN (0, (SELECT oid FROM pg_database
+                                      WHERE datname = current_database()))
+             GROUP BY c.relname
+             ORDER BY 2 DESC
+             LIMIT 10;
+--- No of index and size of it on tables
+
+SELECT
+    pg_class.relname,
+    pg_size_pretty(pg_class.reltuples::BIGINT) AS rows_in_bytes,
+    pg_class.reltuples AS num_rows,
+    COUNT(indexname) AS number_of_indexes,
+    CASE WHEN x.is_unique = 1 THEN 'Y'
+       ELSE 'N'
+    END AS UNIQUE,
+    SUM(CASE WHEN number_of_columns = 1 THEN 1
+              ELSE 0
+            END) AS single_column,
+    SUM(CASE WHEN number_of_columns IS NULL THEN 0
+             WHEN number_of_columns = 1 THEN 0
+             ELSE 1
+           END) AS multi_column
+FROM pg_namespace 
+LEFT OUTER JOIN pg_class ON pg_namespace.oid = pg_class.relnamespace
+LEFT OUTER JOIN
+       (SELECT indrelid,
+           MAX(CAST(indisunique AS INTEGER)) AS is_unique
+       FROM pg_index
+       GROUP BY indrelid) x
+       ON pg_class.oid = x.indrelid
+LEFT OUTER JOIN
+    ( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns FROM pg_index x
+           JOIN pg_class c ON c.oid = x.indrelid
+           JOIN pg_class ipg ON ipg.oid = x.indexrelid  )
+    AS foo
+    ON pg_class.relname = foo.ctablename
+WHERE 
+     pg_namespace.nspname in ('core','sor_t','sor_w')
+AND  pg_class.relkind = 'r'
+GROUP BY pg_class.relname, pg_class.reltuples, x.is_unique
+ORDER BY num_rows desc;
+
+---- To check duplicate coulmn indexs 
+
+SELECT pg_size_pretty(SUM(pg_relation_size(idx))::BIGINT) AS SIZE,
+       (array_agg(idx))[1] AS idx1, (array_agg(idx))[2] AS idx2,
+       (array_agg(idx))[3] AS idx3, (array_agg(idx))[4] AS idx4
+FROM (
+    SELECT indexrelid::regclass AS idx, (indrelid::text ||E'\n'|| indclass::text ||E'\n'|| indkey::text ||E'\n'||
+                                         COALESCE(indexprs::text,'')||E'\n' || COALESCE(indpred::text,'')) AS KEY
+    FROM pg_index) sub
+GROUP BY KEY HAVING COUNT(*)>1
+ORDER BY SUM(pg_relation_size(idx)) DESC;
+
+
+--- check bload in data base
+
+WITH constants AS (
+    -- define some constants for sizes of things
+    -- for reference down the query and easy maintenance
+    SELECT current_setting('block_size')::numeric AS bs, 23 AS hdr, 8 AS ma
+),
+no_stats AS (
+    -- screen out table who have attributes
+    -- which dont have stats, such as JSON
+    SELECT table_schema, table_name, 
+        n_live_tup::numeric as est_rows,
+        pg_table_size(relid)::numeric as table_size
+    FROM information_schema.columns
+        JOIN pg_stat_user_tables as psut
+           ON table_schema = psut.schemaname
+           AND table_name = psut.relname
+        LEFT OUTER JOIN pg_stats
+        ON table_schema = pg_stats.schemaname
+            AND table_name = pg_stats.tablename
+            AND column_name = attname 
+    WHERE attname IS NULL
+        AND table_schema NOT IN ('pg_catalog', 'information_schema')
+    GROUP BY table_schema, table_name, relid, n_live_tup
+),
+null_headers AS (
+    -- calculate null header sizes
+    -- omitting tables which dont have complete stats
+    -- and attributes which aren't visible
+    SELECT
+        hdr+1+(sum(case when null_frac <> 0 THEN 1 else 0 END)/8) as nullhdr,
+        SUM((1-null_frac)*avg_width) as datawidth,
+        MAX(null_frac) as maxfracsum,
+        schemaname,
+        tablename,
+        hdr, ma, bs
+    FROM pg_stats CROSS JOIN constants
+        LEFT OUTER JOIN no_stats
+            ON schemaname = no_stats.table_schema
+            AND tablename = no_stats.table_name
+    WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
+        AND no_stats.table_name IS NULL
+        AND EXISTS ( SELECT 1
+            FROM information_schema.columns
+                WHERE schemaname = columns.table_schema
+                    AND tablename = columns.table_name )
+    GROUP BY schemaname, tablename, hdr, ma, bs
+),
+data_headers AS (
+    -- estimate header and row size
+    SELECT
+        ma, bs, hdr, schemaname, tablename,
+        (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
+        (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
+    FROM null_headers
+),
+table_estimates AS (
+    -- make estimates of how large the table should be
+    -- based on row and page size
+    SELECT schemaname, tablename, bs,
+        reltuples::numeric as est_rows, relpages * bs as table_bytes,
+    CEIL((reltuples*
+            (datahdr + nullhdr2 + 4 + ma -
+                (CASE WHEN datahdr%ma=0
+                    THEN ma ELSE datahdr%ma END)
+                )/(bs-20))) * bs AS expected_bytes,
+        reltoastrelid
+    FROM data_headers
+        JOIN pg_class ON tablename = relname
+        JOIN pg_namespace ON relnamespace = pg_namespace.oid
+            AND schemaname = nspname
+    WHERE pg_class.relkind = 'r'
+),
+estimates_with_toast AS (
+    -- add in estimated TOAST table sizes
+    -- estimate based on 4 toast tuples per page because we dont have 
+    -- anything better.  also append the no_data tables
+    SELECT schemaname, tablename, 
+        TRUE as can_estimate,
+        est_rows,
+        table_bytes + ( coalesce(toast.relpages, 0) * bs ) as table_bytes,
+        expected_bytes + ( ceil( coalesce(toast.reltuples, 0) / 4 ) * bs ) as expected_bytes
+    FROM table_estimates LEFT OUTER JOIN pg_class as toast
+        ON table_estimates.reltoastrelid = toast.oid
+            AND toast.relkind = 't'
+),
+table_estimates_plus AS (
+-- add some extra metadata to the table data
+-- and calculations to be reused
+-- including whether we cant estimate it
+-- or whether we think it might be compressed
+    SELECT current_database() as databasename,
+            schemaname, tablename, can_estimate, 
+            est_rows,
+            CASE WHEN table_bytes > 0
+                THEN table_bytes::NUMERIC
+                ELSE NULL::NUMERIC END
+                AS table_bytes,
+            CASE WHEN expected_bytes > 0 
+                THEN expected_bytes::NUMERIC
+                ELSE NULL::NUMERIC END
+                    AS expected_bytes,
+            CASE WHEN expected_bytes > 0 AND table_bytes > 0
+                AND expected_bytes <= table_bytes
+                THEN (table_bytes - expected_bytes)::NUMERIC
+                ELSE 0::NUMERIC END AS bloat_bytes
+    FROM estimates_with_toast
+    UNION ALL
+    SELECT current_database() as databasename, 
+        table_schema, table_name, FALSE, 
+        est_rows, table_size,
+        NULL::NUMERIC, NULL::NUMERIC
+    FROM no_stats
+),
+bloat_data AS (
+    -- do final math calculations and formatting
+    select current_database() as databasename,
+        schemaname, tablename, can_estimate, 
+        table_bytes, round(table_bytes/(1024^2)::NUMERIC,3) as table_mb,
+        expected_bytes, round(expected_bytes/(1024^2)::NUMERIC,3) as expected_mb,
+        round(bloat_bytes*100/table_bytes) as pct_bloat,
+        round(bloat_bytes/(1024::NUMERIC^2),2) as mb_bloat,
+        table_bytes, expected_bytes, est_rows
+    FROM table_estimates_plus
+)
+-- filter output for bloated tables
+SELECT databasename, schemaname, tablename,
+    can_estimate,
+    est_rows,
+    pct_bloat, mb_bloat,
+    table_mb
+FROM bloat_data
+-- this where clause defines which tables actually appear
+-- in the bloat chart
+-- example below filters for tables which are either 50%
+-- bloated and more than 20mb in size, or more than 25%
+-- bloated and more than 1GB in size
+WHERE ( pct_bloat >= 50 AND mb_bloat >= 20 )
+    OR ( pct_bloat >= 25 AND mb_bloat >= 1000 )
+ORDER BY pct_bloat DESC;
