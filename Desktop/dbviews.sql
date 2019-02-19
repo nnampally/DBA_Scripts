de5005f44b049dabc4b381c8a6c59db4c217c706
nagaraju11 <nraj0429@gmail.com>
2019-02-18 17:45:42 -0800

    Update dbviews.sql
    added blocking locks view

--- Desktop/dbviews.sql
+++ Desktop/dbviews.sql
@@ -1,3 +1,27 @@
+--- view for current transaction blocking locks 
+
+CREATE VIEW lock_monitor AS(
+SELECT
+  COALESCE(blockingl.relation::regclass::text,blockingl.locktype) as locked_item,
+  now() - blockeda.query_start AS waiting_duration, blockeda.pid AS blocked_pid,
+  blockeda.query as blocked_query, blockedl.mode as blocked_mode,
+  blockinga.pid AS blocking_pid, blockinga.query as blocking_query,
+  blockingl.mode as blocking_mode
+FROM pg_catalog.pg_locks blockedl
+JOIN pg_stat_activity blockeda ON blockedl.pid = blockeda.pid
+JOIN pg_catalog.pg_locks blockingl ON(
+  ( (blockingl.transactionid=blockedl.transactionid) OR
+  (blockingl.relation=blockedl.relation AND blockingl.locktype=blockedl.locktype)
+  ) AND blockedl.pid != blockingl.pid)
+JOIN pg_stat_activity blockinga ON blockingl.pid = blockinga.pid
+  AND blockinga.datid = blockeda.datid
+WHERE NOT blockedl.granted
+AND blockinga.datname = current_database()
+);
+
+SELECT * from lock_monitor;
+
+
 -- Table: ddl_events
 
 -- DROP TABLE ddl_events;
