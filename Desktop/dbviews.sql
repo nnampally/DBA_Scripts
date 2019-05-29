860bedc6d24fbe2f0e5457e42e36fc2966edf4ff
nagaraju11 <nraj0429@gmail.com>
2019-05-29 14:26:28 -0700

    Update dbviews.sql

--- Desktop/dbviews.sql
+++ Desktop/dbviews.sql
@@ -194,7 +194,24 @@ CREATE OR REPLACE VIEW dba_tables_sizes AS
   ORDER BY (pg_total_relation_size(c.oid::regclass)) DESC
  LIMIT 20;
 
+--- Query below lists all table columns in a database --
 
+select table_schema,
+       table_name,
+       ordinal_position as position,
+       column_name,
+       data_type,
+       case when character_maximum_length is not null
+            then character_maximum_length
+            else numeric_precision end as max_length,
+       is_nullable,
+       column_default as default_value
+from information_schema.columns
+where table_schema not in ('information_schema', 'pg_catalog')
+order by table_schema, 
+         table_name,
+         ordinal_position;
+                                   
 
 -- View: dba_triggers
 
