7f50074fafd7d3b57702b513302106cfba5e67f9
nagaraju11 <nraj0429@gmail.com>
2020-08-27 12:23:14 -0700

    Update Unused_idx.sql

--- Desktop/Unused_idx.sql
+++ Desktop/Unused_idx.sql
@@ -1,14 +1,17 @@
 SELECT s.schemaname||'.'||s.relname AS tablename,
        s.indexrelname AS indexname,
-       pg_relation_size(s.indexrelid) AS index_size,i_s.indexdef,
+       pg_relation_size(s.indexrelid) AS index_size,
+	   i_s.indexdef,
 	   'DROP INDEX '|| s.schemaname||'.'||s.indexrelname ||';' as DROP_Statment
 FROM pg_catalog.pg_stat_user_indexes s
    JOIN pg_catalog.pg_index i ON s.indexrelid = i.indexrelid
    JOIN pg_catalog.pg_indexes i_s on s.indexrelname = i_s.indexname
 WHERE s.idx_scan = 0      -- has never been scanned
   AND 0 <>ALL (i.indkey)  -- no index column is an expression
+  AND s.schemaname not like '%pg_temp%'
   AND NOT i.indisunique   -- is not a UNIQUE index
   AND NOT EXISTS          -- does not enforce a constraint
          (SELECT 1 FROM pg_catalog.pg_constraint c
           WHERE c.conindid = s.indexrelid)
 ORDER BY pg_relation_size(s.indexrelid) DESC;
+
