593fc18e903332452ef188fc420a54b315294830
nagaraju11 <nraj0429@gmail.com>
2020-08-27 12:13:48 -0700

    Update Unused_idx.sql
    updated with index DDL extraction.

--- Desktop/Unused_idx.sql
+++ Desktop/Unused_idx.sql
@@ -1,9 +1,10 @@
-SELECT s.schemaname,
-       s.relname AS tablename,
+SELECT s.schemaname||'.'||s.relname AS tablename,
        s.indexrelname AS indexname,
-       pg_relation_size(s.indexrelid) AS index_size
+       pg_relation_size(s.indexrelid) AS index_size,i_s.indexdef,
+	   'DROP INDEX '|| s.schemaname||'.'||s.indexrelname ||';' as DROP_Statment
 FROM pg_catalog.pg_stat_user_indexes s
    JOIN pg_catalog.pg_index i ON s.indexrelid = i.indexrelid
+   JOIN pg_catalog.pg_indexes i_s on s.indexrelname = i_s.indexname
 WHERE s.idx_scan = 0      -- has never been scanned
   AND 0 <>ALL (i.indkey)  -- no index column is an expression
   AND NOT i.indisunique   -- is not a UNIQUE index
