e06d67a5545da4e4dd66433e223a013b1c7117e2
nagaraju11 <nraj0429@gmail.com>
2020-09-06 09:49:14 -0700

    Update buffer_query.sql

--- Desktop/buffer_query.sql
+++ Desktop/buffer_query.sql
@@ -31,8 +31,7 @@ LIMIT 10;
 SELECT sum(idx_blks_read) as idx_read, sum(idx_blks_hit)  as idx_hit, (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit) as ratio
 FROM pg_statio_user_indexes;
 
--- all databases and their sizes
-select * from pg_user;
+
 
 -- all tables and their size, with/without indexes
 select datname, pg_size_pretty(pg_database_size(datname))
