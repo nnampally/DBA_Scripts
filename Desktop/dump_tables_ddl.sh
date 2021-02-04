094a85eeb150d4836cf363f339cc8e1e422a61d5
nagaraju11 <nraj0429@gmail.com>
2021-02-04 10:58:03 -0800

    Create dump_tables_ddl.sh

---
+++ Desktop/dump_tables_ddl.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+echo  "Enter schema name: "
+read schema_name
+#echo $schema_name
+echo "Dump type Tables/Functions/Triggers/Views/Materializedview: "
+read sub_dir
+host="localhost"
+user="postgres"
+db="postgres"
+export PGPASSFILE='~/.pgpass'
+export targetdir='<targetdir>'
+
+echo $targetdir
+cd $targetdir
+
+if [ -d $schema_name ]; then
+echo "$schema_name directory already exists";
+else 
+`mkdir -p $targetdir/$schema_name`;
+echo "$schema_name directory created"
+fi
+cd $targetdir/$schema_name
+
+if [ -d $sub_dir ]; then
+echo "$sub_dir directory already exists";
+else 
+`mkdir -p $targetdir/$schema_name/$sub_dir`;
+echo "$sub_dir directory created"
+fi
+
+cd $targetdir/$schema_name/$sub_dir
+
+psql -h $host -U $user -d $db -c  "select schemaname||'.'||tablename as table from pg_tables  where schemaname = '$schema_name' " | while read -a Record ;  do
+ #echo 'helpp'
+ case  "$Record" in 
+ *$schema_name* )
+ #echo $Record
+ #echo "testd"
+  pg_dump -h $host -U $user -d $db -t $Record --schema-only -Fp --no-tablespaces --verbose > $Record.sql
+ ;;
+ esac
+ 
+done
+echo 'completed $targetdir dump'
+
+
