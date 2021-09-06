44b9c719a2f1e05af7de337ad1af1c6209aa52f4
nagaraju11 <nraj0429@gmail.com>
2021-09-05 22:17:08 -0700

    Update function_ddl.sh

--- Desktop/dump_auto/function_ddl.sh
+++ Desktop/dump_auto/function_ddl.sh
@@ -4,12 +4,12 @@ read schema_name
 #echo $schema_name
 echo "Dump type tables/functions/sequences/triggers/views/materializedview: "
 read sub_dir
-host="tmo-eds-prd-hyperlake-cluster.cluster-ro-crondmz09l1n.us-west-2.rds.amazonaws.com"
+host="<>"
 echo $host
 user="edsadmin"
 db="ohl"
-export PGPASSFILE='/Users/nampally/.pgpass' #/Users/nampally/.pgpass
-export targetdir='/Users/nampally/Desktop/OHL-Postgres'
+export PGPASSFILE='/Users/.pgpass' 
+export targetdir='/Users/Desktop/OHL-Postgres'
 echo $targetdir
 cd $targetdir
 
@@ -46,12 +46,12 @@ CREATE SEQUENCE  $schema_name.'||sequence_name || ';
 
 ALTER SEQUENCE $schema_name.' || sequence_name||
     ' 
-    OWNER TO gp_eds_owner;
+    OWNER TO <>;
     
 
-GRANT ALL ON SEQUENCE $schema_name.'|| sequence_name || ' TO gp_eds_etl;
+GRANT ALL ON SEQUENCE $schema_name.'|| sequence_name || ' TO <>;
 
-GRANT ALL ON SEQUENCE $schema_name.'|| sequence_name || ' TO gp_eds_owner;' 
+GRANT ALL ON SEQUENCE $schema_name.'|| sequence_name || ' TO <>;' 
 
 from information_schema.sequences where  sequence_name = '$Record1' 
 and sequence_schema = '$schema_name';" >  "$schema_name.$Record1().sql" 
