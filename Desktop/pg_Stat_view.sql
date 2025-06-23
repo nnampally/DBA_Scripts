0ed17315de9ffbc910a5b330435de7ffcdb3dc69
Nagaraju Nampally <nraj0429@gmail.com>
2025-06-23 16:42:29 -0700

    Update pg_Stat_view.sql

--- Desktop/pg_Stat_view.sql
+++ Desktop/pg_Stat_view.sql
@@ -1,14 +1,14 @@
 SELECT DISTINCT D.DATNAME AS DB_NAME,
 	R.ROLNAME AS USERNAME,
-	S.QUERY,
-	S.CALLS
-FROM DBADMIN.PG_STAT_STATEMENTS S,
+	s.calls,s.max_time ,s.mean_time,s.total_time,s.stddev_time, s.rows ,s.shared_blks_hit , s.shared_blks_read,s.temp_blks_written, s.query
+FROM PG_STAT_STATEMENTS S,
 	PG_ROLES R,
 	PG_DATABASE D
 WHERE S.USERID = R.OID
-				AND S.DBID = D.OID
-				AND D.DATNAME not in ('postgres',
-																											'rdsadmin',
-																											'template')
-				AND QUERY like '%sec_user_organization%'
-				AND QUERY NOT LIKE '%pg_stat_Statements%'
+AND S.DBID = D.OID
+AND D.DATNAME not in ('postgres','rdsadmin') 
+AND D.datallowconn = true
+AND QUERY NOT ILIKE '%pg_stat_Statements%'
+and s.total_time > 1
+order by s.calls desc ,  s.max_time desc, s.total_time desc;
+
